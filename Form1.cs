1e13a29bea525cd1aac933c4468b8ba3fc00761a
Linca <666linca@gmail.com>
2025-02-04 21:37:09 -0300

    Arquivo pedir

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Pedir
+{
+    public partial class frmFormsPedido : Form
+    {
+        public frmFormsPedido()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFormsPedido_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       //Botão que serve pra pedir ingredientes selecionados e etc...
+        private void btnPedir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clbPedidoRapido_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clbIngredientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clbBebidas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
